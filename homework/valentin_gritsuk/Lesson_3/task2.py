ead79d78d0de931e761a204df8982b7d5a7f8197
ValentinGritsuk <marselkaru.com@gmail.com>
2025-03-10 23:46:47 +0300

    Lesson_3_Task 1,2,3,4 are done

---
+++ homework/valentin_gritsuk/Lesson_3/task2.py
@@ -0,0 +1,4 @@
+x = 21
+y = 32
+print(f'x = {x}\ny = {y}\nx - y / 1 + xy = '
+      f'{x} - {y / 1} + {x * y} = {x - y / 1 + x * y}')
---
+++ homework/valentin_gritsuk/Lesson_3/task4.py
@@ -0,0 +1,7 @@
+cat1 = 3
+cat2 = 4
+print(f'''cat1 = 3
+cat2 = 4
+hyp = (cat1^2 + cat2^2) ** (1 / 2) = ({cat1}^2 + {cat2}^2) ** (1 / 2) = {(cat1 ** 2 + cat2 ** 2) ** (1 / 2)}
+Square = cat1 * cat2 = {cat1} * {cat2} = {cat1 * cat2} 
+''')
