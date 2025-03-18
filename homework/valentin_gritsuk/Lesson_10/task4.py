ac93f39bf9e32c84c411a678769a5c2ee74077e9
ValentinGritsuk <marselkaru.com@gmail.com>
2025-03-19 00:27:17 +0300

    Lesson_10 Tasks_1,2,3,4 done

---
+++ homework/valentin_gritsuk/Lesson_10/task4.py
@@ -0,0 +1,9 @@
+PRICE_LIST = '''тетрадь 50р
+книга 200р
+ручка 100р
+карандаш 70р
+альбом 120р
+пенал 300р
+рюкзак 500р'''
+new_dict = {good.split()[0]: int(good.split()[1].replace('р','')) for good in PRICE_LIST.split('\n')}
+print(new_dict)
