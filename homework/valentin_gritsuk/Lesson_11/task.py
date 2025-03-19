a22cdfa910b95fbf52e1eda0ce9260dc6f470c98
ValentinGritsuk <marselkaru.com@gmail.com>
2025-03-19 22:37:02 +0300

    f f

--- homework/valentin_gritsuk/Lesson_11/task.py
+++ homework/valentin_gritsuk/Lesson_11/task.py
@@ -24,14 +24,14 @@ class SchoolTextbook(Book):
         self.tasks_are_in = tasks_are_in
 
     def about_me_print(self):
-        text = (super().about_me().replace(', зарезервирована','').
+        text = (super().about_me().replace(', зарезервирована', '').
                 replace(f'материал: {self.material_of_page}', ''))
-        text  += f'предмет: {self.subject}, класс: {self.class_grade}{", зарезервирована" if self.is_reserved else ""}'
+        text += f'предмет: {self.subject}, класс: {self.class_grade}{", зарезервирована" if self.is_reserved else ""}'
         print(text)
 
 
 first_book = Book('Кот в сапогах', 'Андерсен', 26, 123124124, False)
-second_book =  Book('Идиот', 'Достоевский', 245, 4214123, False)
+second_book = Book('Идиот', 'Достоевский', 245, 4214123, False)
 third_book = Book('Война и мир', 'Толстой', 3253342434, 42342, False)
 fourth_book = Book('Алые паруса', 'Грин', 54, 2341234, False)
 fifth_book = Book('Журавлиный крик', 'Быков', 62, 978985, False)
@@ -42,12 +42,18 @@ print(third_book.about_me())
 print(fourth_book.about_me())
 print(fifth_book.about_me())
 
-math_book = SchoolTextbook('Алгебра', 'Герасимов', '43', '13-4124-124',
-                           False, 'Математика', '10', True)
-english_book = SchoolTextbook('Язык', 'Петичкин', '21', '123542-124',
-                           False, 'Русский', '5', True)
-russ_book = SchoolTextbook('Язык', 'Калинин', '64', '53-41432-122',
-                           False, 'Английский', '11', True)
+math_book = SchoolTextbook(
+    'Алгебра', 'Герасимов', '43', '13-4124-124',
+    False, 'Математика', '10', True
+)
+english_book = SchoolTextbook(
+    'Язык', 'Петичкин', '21', '123542-124',
+    False, 'Русский', '5', True
+)
+russ_book = SchoolTextbook(
+    'Язык', 'Калинин', '64', '53-41432-122',
+    False, 'Английский', '11', True
+)
 math_book.is_reserved = True
 math_book.about_me_print()
 english_book.about_me_print()
