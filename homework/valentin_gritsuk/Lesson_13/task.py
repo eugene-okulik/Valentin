cbdadb519738697713263369e65e2a8fd00a5fca
ValentinGritsuk <marselkaru.com@gmail.com>
2025-03-23 11:53:33 +0300

    Lesson_13 Task_done

---
+++ homework/valentin_gritsuk/Lesson_13/task.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+import locale
+
+
+def read_file(path):
+    with open(path, 'r', encoding='utf-8') as data_file:
+        for line in data_file.readlines():
+            yield line
+
+
+base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+data_path = os.path.join(base_path, 'eugene_okulik', 'hw_13', 'data.txt')
+print(data_path)
+
+for data_line in read_file(data_path):
+    time = (data_line[3 : (data_line.index('распечатать') - 3)])
+    time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+    if data_line[0] == '1':
+        print(time + datetime.timedelta(days=7))
+    elif data_line[0] == '2':
+        locale.setlocale(locale.LC_TIME, 'Russian_Russia.1251')
+        print(time.strftime("%A"))
+    elif data_line[0] == '3':
+        print((datetime.datetime.now() - time).days)
