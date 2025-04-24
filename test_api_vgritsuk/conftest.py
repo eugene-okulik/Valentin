6da46795a85b69d77d07c34cdb0bc27ccee3aadb
ValentinGritsuk <marselkaru.com@gmail.com>
2025-04-25 01:21:45 +0300

    format fix

--- test_api_vgritsuk/conftest.py
+++ test_api_vgritsuk/conftest.py
@@ -13,21 +13,26 @@ DATA = {"name": "first", "data": {"color": "red", "size": "small"}}
 def create_data_endpoint():
     return CreatePost()
 
+
 @pytest.fixture()
 def delete_data_endpoint():
     return DeleteData()
+
 
 @pytest.fixture()
 def get_data_endpoint():
     return GetData()
 
+
 @pytest.fixture()
 def update_data_put_endpoint():
     return UpdateDataPut()
 
+
 @pytest.fixture()
 def update_data_patch_endpoint():
     return UpdateDataPatch()
+
 
 @pytest.fixture()
 def prepared_data(create_data_endpoint, delete_data_endpoint):
--- test_api_vgritsuk/endpoints/update_data_patch.py
+++ test_api_vgritsuk/endpoints/update_data_patch.py
@@ -7,7 +7,7 @@ class UpdateDataPatch(Endpoint):
 
     @allure.step('Update name and/or data PATCH')
     def update_data_patch(self, body, prepared_json):
-        url_with_id = f'{self.url}/{prepared_json['id']}'
+        url_with_id = f"{self.url}/{prepared_json['id']}"
         self.started_json = prepared_json
         self.response = requests.patch(url_with_id, json=body)
         self.json = self.response.json()
